97c792187dd290d61b90ee99fe2b22ad1570dc83
egunde <112527936+egunde@users.noreply.github.com>
2024-01-30 19:07:49 -0600

    Updated Navbar Spacing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,16 +12,33 @@ export default function Navbar() {
     const cartLinkHref = `${pathname}?showCart=true`;
 
     return (
-        <nav className="sticky top-0 z-20 bg-slate-900 text-white text-2xl py-2 px-8 md:px-32 lg:px-96 flex justify-between items-center">
+        <nav className="sticky top-0 z-20 bg-slate-900 text-white text-2xl py-2 px-8 md:px-32 lg:px-96 flex justify-center items-center">
             <Link href="/">
                 <Logo height="50" />
             </Link>
-            {!showCart && (
-                <Link href={cartLinkHref}>
-                    Cart
-                </Link>
-            )}
+            <div
+                className="w-1/3"
+            />
+            <Link href={cartLinkHref}>
+                Cart
+            </Link>
             {showCart && <Cart />}
         </nav>
     );
 }
+// screens: {
+//       'sm': '640px',
+//       // => @media (min-width: 640px) { ... }
+
+//       'md': '768px',
+//       // => @media (min-width: 768px) { ... }
+
+//       'lg': '1024px',
+//       // => @media (min-width: 1024px) { ... }
+
+//       'xl': '1280px',
+//       // => @media (min-width: 1280px) { ... }
+
+//       '2xl': '1536px',
+//       // => @media (min-width: 1536px) { ... }
+//     }
